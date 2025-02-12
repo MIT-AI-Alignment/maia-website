2b8e66b9fabe4037d2dba6bf7ed45f90a6045092
GatlenCulp <GatlenCulp@gmail.com>
2025-02-12 10:18:40 -0500

    feat: update image paths and comment out exec sections in about page

--- src/routes/about/execs.svelte
+++ src/routes/about/execs.svelte
@@ -7,22 +7,22 @@
 
 <div class="flex my-6 flex-wrap">
 	<UpdateNotification message="🌹 To be updated Spring 2025! 🌹" />
-	{#each ACTIVE_EXECS as exec}
+	<!-- {#each ACTIVE_EXECS as exec}
 		<Profile
 			name={exec.name}
 			position={exec.position}
 			imageUrl={exec.imageUrl}
 		/>
-	{/each}
+	{/each} -->
 </div>
 
-<h3>Executives on Temporary Leave</h3>
+<!-- <h3>Executives on Temporary Leave</h3> -->
 <div class="flex my-6 flex-wrap">
-	{#each TEMP_LEAVE_EXECS as exec}
+	<!-- {#each TEMP_LEAVE_EXECS as exec}
 		<Profile
 			name={exec.name}
 			position={exec.position}
 			imageUrl={exec.imageUrl}
 		/>
-	{/each}
+	{/each} -->
 </div>
